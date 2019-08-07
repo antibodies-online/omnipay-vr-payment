e5425af47ef72836d60cdad18a74251722ff2e51
DemigodCode <info@bl-websolutions.de>
2019-08-07 17:17:11 +0200

    Drop 5.6 support

--- tests/Message/NotificationServerRequestTest.php
+++ tests/Message/NotificationServerRequestTest.php
@@ -22,13 +22,7 @@ class NotificationServerRequestTest extends TestCase
         $this->content = json_decode($json, true);
 
         $this->iv = openssl_random_pseudo_bytes(16);
-        if(PHP_VERSION_ID >= 70100) {
-            $encrypted = openssl_encrypt($json, 'aes-256-gcm', $this->key, OPENSSL_RAW_DATA, $this->iv, $this->auth_tag);
-        } else {
-            $this->iv = \Sodium\randombytes_buf(\Sodium\CRYPTO_AEAD_AES256GCM_NPUBBYTES);
-            $this->key = \Sodium\randombytes_buf(\Sodium\CRYPTO_AEAD_AES256GCM_KEYBYTES);
-            $encrypted = \Sodium\crypto_aead_aes256gcm_encrypt($json . $this->auth_tag, $this->iv, $this->key);
-        }
+        $encrypted = openssl_encrypt($json, 'aes-256-gcm', $this->key, OPENSSL_RAW_DATA, $this->iv, $this->auth_tag);
 
         $request = new Request([], [], [], [], [], ['HTTP_X-Initialization-Vector' => bin2hex($this->iv), 'HTTP_X-Authentication-Tag' => bin2hex($this->auth_tag)], bin2hex($encrypted));
         $this->gateway = new VrPaymentGateway($this->getHttpClient(), $request);
