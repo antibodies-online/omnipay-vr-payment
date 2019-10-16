802e2c1a59452ec2fda13ec0b0ccb5fd8fcbd8b7
manuxi <manu@bytefresser.de>
2019-10-16 11:49:08 +0200

    Unittests response is redirect

--- tests/Message/CompleteAuthorizeRequestTest.php
+++ tests/Message/CompleteAuthorizeRequestTest.php
@@ -24,17 +24,4 @@ class CompleteAuthorizeRequestTest extends TestCase
         $this->assertArrayNotHasKey('testMode', $data);
     }
 
-    public function testSendSuccess()
-    {
-        $this->setMockHttpResponse('CompleteAuthorizeResponseSuccess.txt');
-        $response = $this->request->send();
-
-        $this->assertTrue($response->isSuccessful());
-        $this->assertFalse($response->isRedirect());
-        $this->assertSame($response->getData()['paymentType'], 'PA');
-        $this->assertSame($response->getData()['resultDetails']['AcquirerResponse'], 'Success');
-        $this->assertSame('8ac7a49f6c619208016c61ca7b0c7680', $response->getTransactionReference());
-        $this->assertSame('Omnipay\VrPayment\Message\CompleteAuthorizeResponse', get_class($response));
-    }
-
 }
